74f93a3330aa0596f0203c7a95fa91abce34d474
Abraham Kaczmarski <abrahamkaczmarski@gmail.com>
2021-05-04 17:05:59 +0200

    Add course list display

--- adaptive-system-front/src/views/Home.js
+++ adaptive-system-front/src/views/Home.js
@@ -1,12 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { getCourseList } from '../services/courses'
 
 
-const Home = async () =>
+const Home = () =>
 {
+    const [courseList, setCourseList] = useState([])
+    useEffect(async () =>
+    {
+        let res = await getCourseList()
+        setCourseList(res.data)
+    })
+
     return (
         <div>
             <h2>Strona główna</h2>
+            <ul>
+                {courseList.map(course => (<li>{course.name}</li>))}
+            </ul>
         </div>
     )
 }
