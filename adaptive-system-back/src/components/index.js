99099a7bc913b4ee9cbff8f3d95d0defee4baa14
Justyanna <annajkuczynska@onet.pl>
2021-05-04 12:56:59 +0200

    typo fix

--- adaptive-system-back/src/components/index.js
+++ adaptive-system-back/src/components/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import usersRouter from './users/users.router.js';
-import courseRoter from './courses/coursers.router.js';
+import courseRoter from './courses/courses.router.js';
 
 const main = express.Router();
 const componentRouters = [ usersRouter, courseRoter ];
