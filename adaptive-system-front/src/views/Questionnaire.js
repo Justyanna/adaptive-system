6758140c8dcc6cbb1ed223b523fb19a1427ec137
Abraham Kaczmarski <abrahamkaczmarski@gmail.com>
2021-05-16 20:30:30 +0200

    Display questionnaire questions

--- adaptive-system-front/src/views/Questionnaire.js
+++ adaptive-system-front/src/views/Questionnaire.js
@@ -1,15 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { getQuestions } from '../services/users'
 
 const Questionnaire = () => {
+  let [questions, setQuestions] = useState(null)
+
   useEffect(() => {
     ;(async () => {
       let res = await getQuestions()
-      console.log(res)
+      setQuestions(res.data.questions)
     })()
   }, [])
 
-  return <div>Ankieta</div>
+  return (
+    <div>
+      <h2>Strona główna</h2>
+      {questions === null ? (
+        <p>Nie ma jeszcze żadnych kursów</p>
+      ) : (
+        <ul className="list">
+          {questions.map((question, id) => (
+            <li className="list-item" key={id}>
+              {question}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  )
 }
 
 export default Questionnaire
