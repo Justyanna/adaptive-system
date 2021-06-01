a3805fbd91099949c06ad24575affad76c3dffa8
Justyanna <annajkuczynska@onet.pl>
2021-06-01 11:30:50 +0200

    change files names to uuids

--- adaptive-system-back/src/utils/fileStorage.js
+++ adaptive-system-back/src/utils/fileStorage.js
@@ -1,12 +1,13 @@
 import multer from 'multer';
 import path from 'path';
+import { v4 as uuidv4 } from 'uuid';
 
 const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, path.join(path.resolve(), '/uploads'));
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now() + '--' + file.originalname);
+        cb(null, uuidv4());
     }
 });
 
