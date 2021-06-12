30b5fada1fa95a70c60088f2db79fc4f12e4c2c0
Abraham Kaczmarski <abrahamkaczmarski@gmail.com>
2021-06-12 19:35:18 +0200

    Add image endpoint

--- adaptive-system-back/src/components/files/files.router.js
+++ adaptive-system-back/src/components/files/files.router.js
@@ -1,9 +1,15 @@
-import express from 'express';
-import filesController from './files.controller.js';
-import auth from '../../middleware/auth.js';
-import upload from '../../utils/fileStorage.js';
+import express from 'express'
+import path from 'path'
+import filesController from './files.controller.js'
+import auth from '../../middleware/auth.js'
+import upload from '../../utils/fileStorage.js'
 
-const filesRouter = express.Router();
-filesRouter.post('/file', upload.single('file'), filesController.uploadFile);
-filesRouter.get('/file/:fileId', auth.authenticate, filesController.getFile);
-export default filesRouter;
+const filesRouter = express.Router()
+filesRouter.post('/file', upload.single('file'), filesController.uploadFile)
+filesRouter.get('/file/:fileId', auth.authenticate, filesController.getFile)
+
+filesRouter.get('/img', function (req, res) {
+  res.sendFile(path.join(path.resolve(), 'uploads/plan_zajec.png'))
+})
+
+export default filesRouter
